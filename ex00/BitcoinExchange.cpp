f106e04db10a1d8540e0e2a18d1486e7f9aa22dd
Emad Aldeen Hammoude <ehammoud@lab1r1s5.42abudhabi.ae>
2024-12-10 21:38:57 +0400

    added

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,17 +1,69 @@
 #include "BitcoinExchange.hpp"
 
-BitcoinExchange::BitcoinExchange()
+BitcoinExchange::BitcoinExchange(): database()
 {
-	std::cout << "An BitcoinExchange has been created!\n";
+
 }
 
-BitcoinExchange::BitcoinExchange(const BitcoinExchange &copy)
+BitcoinExchange::BitcoinExchange(str database): database()
 {
-	std::cout << "An BitcoinExchange has been created with the same qualities as another!\n";
+	if (!validFileExtension(database, ".csv"))
+		throw std::runtime_error("Invalid file extension! Expected extension (.csv)");
+
 }
+
+BitcoinExchange::BitcoinExchange(str database, str inputfile): database()
+{
+	if (!validFileExtension(database, ".csv"))
+		throw std::runtime_error("Invalid file extension! Expected extension (.csv)");
+	if (!validFileExtension(inputfile, ".txt"))
+		throw std::runtime_error("Invalid file extension! Expected extension (.txt)");
+
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &copy): database(copy.database)
+{
+	
+}
+
+bool BitcoinExchange::validFileExtension(str filename, str extension) const
+{
+	if (filename.length() < extension.length())
+		return false;
+	return (filename.substr(filename.length() - extension.length(), extension.length()) == extension);
+}
+
+void BitcoinExchange::parseInput(str inputfile)
+{
+	if (!validFileExtension(inputfile, ".txt"))
+		throw std::runtime_error("Invalid file extension in (" + inputfile + ")! Expected extension (.txt)");
+	if (database.empty())
+	{
+		std::cout << "BTC -> No Database is loaded in this object!";
+		return ;
+	}
+}
+
+void BitcoinExchange::parseDatabase(str database)
+{
+	if (!validFileExtension(database, ".csv"))
+		throw std::runtime_error("Invalid file extension (" + database + ")! Expected extension (.csv)");
+	if (!database.empty())
+	{
+		std::cout << "BTC -> A Database is already loaded in this object!";
+		return ;
+	}
+}
+
+float BitcoinExchange::getExchangeRate(str date) const
+{
+
+}
+
 
 BitcoinExchange &BitcoinExchange::operator =(const BitcoinExchange &copy)
 {
+	(void) copy;
 	//if (&copy != this)
 		//copy
 	return *this;
@@ -19,5 +71,5 @@ BitcoinExchange &BitcoinExchange::operator =(const BitcoinExchange &copy)
 
 BitcoinExchange::~BitcoinExchange()
 {
-	std::cout << "The BitcoinExchange has been destroyed!\n";
+	
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "BitcoinExchange.hpp"
 
-int main()
+int main(int ac, char **av)
 {
-
+	if (ac != 2)
+	{
+		std::cerr << "Error: Incorrect number of arguments!\n";
+		std::cerr << "Usage: ./btc <filename.txt>";
+		return (1);
+	}
+	
 	return (0);
 }
